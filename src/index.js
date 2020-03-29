77df2ab06159cfbddcdba6ae597193d4d0aceeaa
anupama <anupamagadde@yahoo.com>
2020-03-29 08:34:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  createTaskForm.addEventListener("submit", addTaskToDOM);
 });
+ 
+function addTaskToDOM(event) {
+    event.preventDefault()
+    console.log("form submit", event);
+    const newTaskDescriptionInput = document.getElementById("new-task-description");
+    const newTaskDescription = newTaskDescriptionInput.value;
+    tasks.innerHTML += `<li>${newTaskDescription}</li>`;
+    newTaskDescription.value = "";
+}
